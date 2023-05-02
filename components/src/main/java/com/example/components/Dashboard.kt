5a0a9d91a1e1e0bed064f352d29a0fd46125dc01
Priyanshu <sahupriyanshu108@gmail.com>
2023-05-02 23:46:24 +0530

    added Fragment Dashboard

---
+++ components/src/main/java/com/example/components/Dashboard.kt
@@ -0,0 +1,27 @@
+package com.example.components
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+
+
+class Dashboard : Fragment(){
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        Toast.makeText(context,"Dashboard",Toast.LENGTH_SHORT).show()
+        return binding
+    }
+
+
+}
--- studentapp/src/main/java/com/example/studentapp/SplashActivity.kt
+++ studentapp/src/main/java/com/example/studentapp/SplashActivity.kt
@@ -1,17 +1,20 @@
-package com.example.attendanceapp
+package com.example.studentapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.components.Dashboard
-
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
 
-
+        val dashboardFragment = Dashboard()
+        fragmentTransaction.add(R.id.container, dashboardFragment)
+        fragmentTransaction.commit()
     }
 }
